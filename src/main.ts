ed153ccdf762b072f8cfc23a581bb24654776c29
Luke Chadwick <luke@vertis.io>
2022-07-24 09:15:51 +0200

    Basic postprocessor

---
+++ src/main.ts
@@ -0,0 +1,22 @@
+import { Plugin } from "obsidian";
+
+export default class BetterGalleryPlugin extends Plugin {
+  async onload() {
+    this.registerMarkdownCodeBlockProcessor("better-gallery", (source, el, ctx) => {
+      const rows = source.split("\n").filter((row) => row.length > 0);
+
+      const table = el.createEl("table");
+      const body = table.createEl("tbody");
+
+      for (let i = 0; i < rows.length; i++) {
+        const cols = rows[i].split(",");
+
+        const row = body.createEl("tr");
+
+        for (let j = 0; j < cols.length; j++) {
+          row.createEl("td", { text: cols[j] });
+        }
+      }
+    });
+  }
+}
