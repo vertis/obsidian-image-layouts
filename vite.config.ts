4c67512bbf994de263e8681162e4bc0bfabc89ac
Luke Chadwick <luke@vertis.io>
2024-01-27 17:15:08 +0200

    chore: tweak the build to use the correct plugin name

--- vite.config.ts
+++ vite.config.ts
@@ -5,53 +5,53 @@ import UnoCSS from "unocss/vite";
 import { PluginOption, defineConfig } from "vite";
 
 const setOutDir = (mode: string) => {
-	switch (mode) {
-		case "development":
-			return "./test-vault/.obsidian/plugins/obsidian-svelte-plugin";
-		case "production":
-			return "build";
-	}
+  switch (mode) {
+    case "development":
+      return "./test-vault/.obsidian/plugins/obsidian-image-layouts";
+    case "production":
+      return "build";
+  }
 };
 
 export default defineConfig(({ mode }) => {
-	return {
-		plugins: [
-			UnoCSS(),
-			svelte({ preprocess: vitePreprocess() }) as PluginOption,
-		],
-		build: {
-			lib: {
-				entry: "src/main",
-				formats: ["cjs"],
-			},
-			rollupOptions: {
-				output: {
-					entryFileNames: "main.js",
-					assetFileNames: "styles.css",
-					sourcemapBaseUrl: pathToFileURL(
-						`${__dirname}/test-vault/.obsidian/plugins/obsidian-svelte-plugin/`,
-					).toString(),
-				},
-				external: [
-					"obsidian",
-					"electron",
-					"@codemirror/autocomplete",
-					"@codemirror/collab",
-					"@codemirror/commands",
-					"@codemirror/language",
-					"@codemirror/lint",
-					"@codemirror/search",
-					"@codemirror/state",
-					"@codemirror/view",
-					"@lezer/common",
-					"@lezer/highlight",
-					"@lezer/lr",
-					...builtins,
-				],
-			},
-			outDir: setOutDir(mode),
-			emptyOutDir: false,
-			sourcemap: "inline",
-		},
-	};
+  return {
+    plugins: [
+      UnoCSS(),
+      svelte({ preprocess: vitePreprocess() }) as PluginOption,
+    ],
+    build: {
+      lib: {
+        entry: "src/main",
+        formats: ["cjs"],
+      },
+      rollupOptions: {
+        output: {
+          entryFileNames: "main.js",
+          assetFileNames: "styles.css",
+          sourcemapBaseUrl: pathToFileURL(
+            `${__dirname}/test-vault/.obsidian/plugins/obsidian-image-layouts/`
+          ).toString(),
+        },
+        external: [
+          "obsidian",
+          "electron",
+          "@codemirror/autocomplete",
+          "@codemirror/collab",
+          "@codemirror/commands",
+          "@codemirror/language",
+          "@codemirror/lint",
+          "@codemirror/search",
+          "@codemirror/state",
+          "@codemirror/view",
+          "@lezer/common",
+          "@lezer/highlight",
+          "@lezer/lr",
+          ...builtins,
+        ],
+      },
+      outDir: setOutDir(mode),
+      emptyOutDir: false,
+      sourcemap: "inline",
+    },
+  };
 });
