89247739b7e75b3c76a47527c271e47fd5b49c41
Luke Chadwick <luke@vertis.io>
2024-01-27 17:00:55 +0200

    refactor: import https://github.com/emilio-toledo/obsidian-svelte-plugin

---
+++ old/bin/copy-to-main.fish
@@ -0,0 +1,3 @@
+#!/usr/bin/env fish
+mkdir -p ~/Dropbox/Apps/Obsidian/Main/.obsidian/plugins/obsidian-image-layouts
+cp main.js styles.css manifest.json ~/Dropbox/Apps/Obsidian/Main/.obsidian/plugins/obsidian-image-layouts
