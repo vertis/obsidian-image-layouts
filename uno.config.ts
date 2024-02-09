6c9de443b4ef34bec86b9cf88f31b6cb495dfe03
Luke Chadwick <luke@vertis.io>
2024-02-09 18:34:20 +0200

    fix: captions don't work in darkmode

--- uno.config.ts
+++ uno.config.ts
@@ -3,6 +3,10 @@ import extractorSvelte from "@unocss/extractor-svelte";
 import presetUno from "@unocss/preset-uno";
 
 export default defineConfig({
-	presets: [presetUno()],
-	extractors: [extractorSvelte()],
+  presets: [
+    presetUno({
+      dark: { dark: ".theme-dark", light: ".theme-light" },
+    }),
+  ],
+  extractors: [extractorSvelte()],
 });
