410fb01bed4923dd5b42de5e8565f687d8ec1e7b
Luke Chadwick <luke@vertis.io>
2024-02-11 17:03:48 +0200

    revert: all the poly fills

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import { svelte, vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 import builtins from "builtin-modules";
 import UnoCSS from "unocss/vite";
 import { PluginOption, defineConfig } from "vite";
-import nodePolyfills from "rollup-plugin-polyfill-node";
 
 const setOutDir = (mode: string) => {
   switch (mode) {
@@ -17,7 +16,6 @@ const setOutDir = (mode: string) => {
 export default defineConfig(({ mode }) => {
   return {
     plugins: [
-      nodePolyfills(),
       UnoCSS(),
       svelte({ preprocess: vitePreprocess() }) as PluginOption,
     ],
