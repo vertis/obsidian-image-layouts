5eb98112bee42d724927b0b304a8248a2d064475
Luke Chadwick <luke@vertis.io>
2026-07-07 00:47:36 +0200

    test: fix stale image parser assertions and expand coverage
    The suite predated the alt field on parsed image links and had never
    run in CI, so it failed on main.

--- test/images.test.ts
+++ test/images.test.ts
@@ -1,4 +1,4 @@
-import { getImageFromLine } from "../src/utils/images.ts";
+import { getImageFromLine, getImages } from "../src/utils/images.ts";
 import { expect, test } from "vitest";
 
 test("getImageFromLine with external link", () => {
@@ -7,6 +7,17 @@ test("getImageFromLine with external link", () => {
   expect(result).toEqual({
     type: "external",
     link: "http://example.com/image.jpg",
+    alt: "Image",
+  });
+});
+
+test("getImageFromLine with external link without alt", () => {
+  const line = "![](https://example.com/image.jpg)";
+  const result = getImageFromLine(line);
+  expect(result).toEqual({
+    type: "external",
+    link: "https://example.com/image.jpg",
+    alt: "",
   });
 });
 
@@ -16,6 +27,7 @@ test("getImageFromLine with internal link", () => {
   expect(result).toEqual({
     type: "wiki",
     link: "image.jpg",
+    alt: "Image",
   });
 });
 
@@ -26,4 +38,26 @@ test("getImageFromLine with wiki link", () => {
     type: "wiki",
     link: "image.jpg",
   });
+});
+
+test("getImageFromLine with a non-image line", () => {
+  expect(getImageFromLine("just some text")).toBeNull();
+  expect(getImageFromLine("")).toBeNull();
+});
+
+test("getImages collects only embed lines", () => {
+  const source = [
+    "![[one.jpg]]",
+    "some text",
+    "![two](two.jpg)",
+    "",
+    "![three](https://example.com/three.jpg)",
+  ].join("\n");
+  const images = getImages(source);
+  expect(images).toHaveLength(3);
+  expect(images.map((i) => i.link)).toEqual([
+    "one.jpg",
+    "two.jpg",
+    "https://example.com/three.jpg",
+  ]);
 });
